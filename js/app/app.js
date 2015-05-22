bac44723a961523ce0d1dd51af2a9a5030604b2f
maksrit <M.Ovsjannikov@uarank.com>
2015-05-22 17:40:43 +0300

    add angular change css

--- js/app/app.js
+++ js/app/app.js
@@ -1,8 +1,30 @@
-﻿ 
+﻿/*global window*/
+(function (window, angular) {
+    'use strict';
 
+    window.appName = window.appName || 'comix';
+/*
+    angular
+        .module(window.appName, ['ui.router', 'ngRoute'])
+        .config(['$routeProvider', function ($routeProvider) {
+          $routeProvider
+                .when('/index',  { templateUrl: 'partials/home.html', controller: 'homeCtrl' })
+                .when('/social', { templateUrl: 'partials/social.html', controller: 'homeCtrl' })
+                 
+                .otherwise({ redirectTo: '/index' });
+        }]);*/
 
+    angular
+        .module(window.appName, ['ui.router'])
+        .config(['$routeProvider',
+            function ($routeProvider) {
+                $routeProvider
+        .when('/index', { templateUrl: 'partials/home.html', controller: 'mainCtrl' })
+         
+        .otherwise({ redirectTo: '/index' });
+                 
 
-$(document).ready(function () {
+            }
+        ]);
 
-    // $("body").fitText();
-});
+}(window, window.angular));
---
+++ js/mainCtrl.js
@@ -0,0 +1,11 @@
+﻿/*global window */
+(function (window, angular) {
+    'use strict';
+
+    angular
+        .module(window.appName)
+        .controller('mainCtrl', ['$scope', 
+            function ($scope) {
+               
+            }]);
+}(window, window.angular));
