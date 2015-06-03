bd6ba0a51a490ebffbdf0970e60553341fd186f7
maksrit <M.Ovsjannikov@uarank.com>
2015-06-03 16:45:18 +0300

    code refactoring

--- js/controllers/titleCtrl.js
+++ js/controllers/titleCtrl.js
@@ -6,42 +6,7 @@
         .module(window.appName)
         .controller('titleCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
 
-          /*  $scope.popapWidth = $(window).innerWidth();
-            
-            $scope.popapHeight = $(window).innerHeight();
-           
-            
-            $scope.changeRotateVideoPopap = function () {
-                
-                var className = null;
-                
-                var width = $(window).innerWidth();
-                var height = $(window).innerHeight();
-
-                if (width < height) {
-                    className = "rotateVideo90";
-                    $scope.popapWidth = height;
-                    $scope.popapHeight = width;
-
-                } else {
-                    $scope.popapWidth = width;
-                    $scope.popapHeight = height;
-                    className = "rotateVideo";
-                }
-                
-                return className;
-            };
-
-            window.onresize = function (event) {
-                $scope.$apply($scope.changeRotateVideoPopap());
-            };*/
-
-         /*   $timeout(function() {
-                $('#title-page')
-                         .prop('class', 'modal fade') // revert to default
-                         .addClass($(this).data('direction'));
-                $('#title-page').modal('show');
-            }, 500);*/
+    
         } ]);
 
 }(window, window.angular));
