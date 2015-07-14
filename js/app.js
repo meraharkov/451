78d38322dee6d1a40b6e7d1bfce9bc60dccbd30a
maksrit <M.Ovsjannikov@uarank.com>
2015-07-14 15:55:38 +0300

    create derectory

--- js/app.js
+++ js/app.js
@@ -1,29 +1,48 @@
 document.addEventListener("deviceready", init, false);
 function init() {
-	alert("init");
-	//This alias is a read-only pointer to the app itself
-	window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + "www/index.html", gotFile, fail);
-
+   alert("init")
+    //This alias is a read-only pointer to the app itself
+    window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + "www/index.html", gotFile, fail);
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onRequestFileSystemSuccess, null);
+    alert("cordova.file.applicationDirectory" + cordova.file.applicationDirectory);
 }
 
 function fail(e) {
-	alert("FileSystem Error")
-	console.log("FileSystem Error");
-	console.dir(e);
+    alert("FileSystem Error")
+    console.log("FileSystem Error");
+    console.dir(e);
 }
 
 function gotFile(fileEntry) {
 
-	fileEntry.file(function(file) {
-		var s = "";
-		s += "<b>name:</b> " + file.name + "<br/>";
-		s += "<b>localURL:</b> " + file.localURL + "<br/>";
-		s += "<b>type:</b> " + file.type + "<br/>";
-		s += "<b>lastModifiedDate:</b> " + (new Date(file.lastModifiedDate)) + "<br/>";
-		s += "<b>size:</b> " + file.size + "<br/>";
-		
-		document.querySelector("#status").innerHTML = s;
-		console.dir(file);
-		alert(file);
-	});
+    fileEntry.file(function (file) {
+        var s = "";
+        s += "<b>name:</b> " + file.name + "<br/>";
+        s += "<b>localURL:</b> " + file.localURL + "<br/>";
+        s += "<b>type:</b> " + file.type + "<br/>";
+        s += "<b>lastModifiedDate:</b> " + (new Date(file.lastModifiedDate)) + "<br/>";
+        s += "<b>size:</b> " + file.size + "<br/>";
+
+        document.querySelector("#status").innerHTML = s;
+        console.dir(file);
+        alert(file);
+    });
+}
+
+
+function onRequestFileSystemSuccess(fileSystem) {
+    alert("onRequestFileSystemSuccess")
+    alert("fileSystem.root")
+    var entry = fileSystem.root;
+    entry.getDirectory("example", { create: true, exclusive: false }, onGetDirectorySuccess, onGetDirectoryFail);
+}
+
+function onGetDirectorySuccess(dir) {
+    console.log("Created dir " + dir.name);
+    alert("Created dir " + dir.name);
+}
+
+function onGetDirectoryFail(error) {
+    alert("Error creating directory " + error.code);
+    console.log("Error creating directory " + error.code);
 }
