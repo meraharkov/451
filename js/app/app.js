66b116646532a045aff69c20080145437cde05ae
maksrit <M.Ovsjannikov@uarank.com>
2015-07-14 15:18:38 +0300

    test device ready

--- js/app/app.js
+++ js/app/app.js
@@ -143,55 +143,59 @@ $(document).ready(function () {
     }*/
 
    
-    document.addEventListener("deviceready", onDeviceReady, true);
     
-    
-
-    // device APIs are available
-    function onDeviceReady() {
-
-        alert("onDeviceReady");
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
-    }
-
-    function gotFS(fileSystem) {
-        
-        alert(fileSystem.name);
-        alert(fileSystem.root.name);
-        
-
-        fileSystem.root.getFile("readme.txt", null, gotFileEntry, fail);
-    }
-
-    function gotFileEntry(fileEntry) {
-        fileEntry.file(gotFile, fail);
-    }
-
-    function gotFile(file) {
-        readDataUrl(file);
-        readAsText(file);
-    }
-
-    function readDataUrl(file) {
-        var reader = new FileReader();
-        reader.onloadend = function (evt) {
-            alert("Read as data URL");
-            alert(evt.target.result);
-        };
-        reader.readAsDataURL(file);
-    }
-
-    function readAsText(file) {
-        var reader = new FileReader();
-        reader.onloadend = function (evt) {
-            alert("Read as text");
-            alert(evt.target.result);
-        };
-        reader.readAsText(file);
-    }
-
-    function fail(error) {
-        alert(error.code);
-    } 
     
 });
+
+/*document.addEventListener("deviceready", onDeviceReady, true);*/
+
+document.addEventListener("deviceready", function () {
+    alert("123");
+}, true);
+
+// device APIs are available
+function onDeviceReady() {
+
+    alert("onDeviceReady");
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+}
+
+function gotFS(fileSystem) {
+
+    alert(fileSystem.name);
+    alert(fileSystem.root.name);
+
+
+    fileSystem.root.getFile("readme.txt", null, gotFileEntry, fail);
+}
+
+function gotFileEntry(fileEntry) {
+    fileEntry.file(gotFile, fail);
+}
+
+function gotFile(file) {
+    readDataUrl(file);
+    readAsText(file);
+}
+
+function readDataUrl(file) {
+    var reader = new FileReader();
+    reader.onloadend = function (evt) {
+        alert("Read as data URL");
+        alert(evt.target.result);
+    };
+    reader.readAsDataURL(file);
+}
+
+function readAsText(file) {
+    var reader = new FileReader();
+    reader.onloadend = function (evt) {
+        alert("Read as text");
+        alert(evt.target.result);
+    };
+    reader.readAsText(file);
+}
+
+function fail(error) {
+    alert(error.code);
+}
