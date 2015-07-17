e2ce14de0cbae19cb1879d090b1e618270fe8684
maksrit <M.Ovsjannikov@uarank.com>
2015-07-17 15:15:19 +0300

    23x

--- js/app/fileManeger.js
+++ js/app/fileManeger.js
@@ -259,7 +259,7 @@ function download(URL, Folder_Name, File_Name) {
       var createDirectorySuccess = function(entry) {
             alert("Dir path - " + entry.fullPath);
             alert("/"+folderName + " == " + entry.fullPath);
-            if ("/" +  entry.fullPath == folderName) {
+            if ("/" + folderName == entry.fullPath) {
                 alert("start download");
                 
 
