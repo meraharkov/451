144f32c14920bf317be7527296f7b5e1fc57c93b
maksrit <M.Ovsjannikov@uarank.com>
2015-07-20 14:52:42 +0300

    40 separ

--- js/app/fileManeger.js
+++ js/app/fileManeger.js
@@ -37,7 +37,7 @@ function onRequestFileSystemSuccess(fileSystem) {
     alert("download_link " + download_link);
 
    
-    var fp = fileSystem.root.fullPath + folderName + File_Name;
+    var fp = cordova.file.applicationDirectory + folderName + File_Name;  // fileSystem.root.fullPath + folderName + File_Name; // fail 
      // "cdvfile://localhost/persistent/" + folderName + File_Name;
     //fileSystem.root.toURL() + folderName + File_Name; //important workin code
     
