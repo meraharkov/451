a673551de9d8cb1d0da5783ef4a28a9002f361ba
maksrit <M.Ovsjannikov@uarank.com>
2015-07-21 15:47:43 +0300

    devise ready commit

--- js/app/initDevice.js
+++ js/app/initDevice.js
@@ -1,18 +1,29 @@
 ﻿
-alert("init device js")
-document.addEventListener("deviceready", onDeviceReady, false);
+alert("init device js");
+/*document.addEventListener("deviceready", onDeviceReady, false);*/
 window.fileSystemGlobal = null;
 
+document.addEventListener('deviceready', function onDeviceReady() {
+    alert("onDeviceReady");
+    $("#loading-id").append("<div class='log-info'>" + "onDeviceReady " + " </div>");
+
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onRequestFileSystemSuccess, null);
+}, false);
+
+/*
 function onDeviceReady() {
      
     alert("onDeviceReady");
     $("#loading-id").append("<div class='log-info'>" + "onDeviceReady " +  " </div>");
     
     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onRequestFileSystemSuccess, null);
-}
+}*/
 
 function onRequestFileSystemSuccess(fileSystem) {
     alert("onRequestFileSystemSuccess");
     window.fileSystemGlobal = fileSystem;
+
+    alert("root : " + window.fileSystemGlobal.root.toNativeURL());
+    
     $("#loading-id").append("<div class='log-info'>" + "onRequestFileSystemSuccess " + " </div>"); 
 }
